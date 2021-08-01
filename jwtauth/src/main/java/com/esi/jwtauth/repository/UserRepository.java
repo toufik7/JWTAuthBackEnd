a824e14ebca55b7b3896fac7518d2d9f96cbe40e
Benmessabih Toufik <52804863+toufik7@users.noreply.github.com>
2021-08-01 14:53:17 +0100

    Update UserRepository.java

--- jwtauth/src/main/java/com/esi/jwtauth/repository/UserRepository.java
+++ jwtauth/src/main/java/com/esi/jwtauth/repository/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserDAO, Long> {
     UserDAO findByUsername(String username);
+    
+    Collection<UserDAO> findUserDAOByParent(UserDAO parent);
+
 }
