fffac24268fd60a7ca64417c133bd54101724181
Benmessabih Toufik <52804863+toufik7@users.noreply.github.com>
2021-08-02 22:24:43 +0100

    Add files via upload

--- jwtauth/src/main/java/com/esi/jwtauth/JwtauthApplication.java
+++ jwtauth/src/main/java/com/esi/jwtauth/JwtauthApplication.java
@@ -2,8 +2,10 @@ package com.esi.jwtauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class JwtauthApplication {
 
     public static void main(String[] args) {
