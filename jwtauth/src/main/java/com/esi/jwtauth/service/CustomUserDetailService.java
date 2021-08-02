2d226c3297591a1e6294d795b3b7a805ab128e30
Benmessabih Toufik <52804863+toufik7@users.noreply.github.com>
2021-08-02 09:16:28 +0100

    Update CustomUserDetailService.java

--- jwtauth/src/main/java/com/esi/jwtauth/service/CustomUserDetailService.java
+++ jwtauth/src/main/java/com/esi/jwtauth/service/CustomUserDetailService.java
@@ -38,29 +38,32 @@ public class CustomUserDetailService implements UserDetailsService {
                     throw new DisabledException("User Disabled : " + username);
             }
         }
-/*
-        if(username.equals("admin"))
-        {
-            roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
-            return new User("admin", "$2y$12$I0Di/vfUL6nqwVbrvItFVOXA1L9OW9kLwe.1qDPhFzIJBpWl76PAe",
-                    roles);
-        }
-        else if(username.equals("user"))
-        {
-            roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-            return new User("user", "$2y$12$VfZTUu/Yl5v7dAmfuxWU8uRfBKExHBWT1Iqi.s33727NoxHrbZ/h2",
-                    roles);
-        }*/
 
         throw new UsernameNotFoundException("User not found with username: " + username);
     }
+    
+    
 
     public UserDAO save(UserDTO user) {
-        UserDAO newUser = new UserDAO();
-        newUser.setUsername(user.getUsername());
-        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
-        newUser.setRole(user.getRole());
-        newUser.setState(user.getState());
-        return userDao.save(newUser);
+
+        if (user.getRole().equals("ROLE_PATIENT")){
+            Patient newPatient = new Patient();
+            newPatient.setUsername(user.getUsername());
+            newPatient.setPassword(bcryptEncoder.encode(user.getPassword()));
+            newPatient.setRole(user.getRole());
+            newPatient.setState(user.getState());
+            newPatient.setParent(user.getParent());
+            newPatient.setChildren(user.getChildren());
+            return userDao.save(newPatient);
+        }else {
+            UserDAO newUser = new UserDAO();
+            newUser.setUsername(user.getUsername());
+            newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+            newUser.setRole(user.getRole());
+            newUser.setState(user.getState());
+            newUser.setParent(user.getParent());
+            newUser.setChildren(user.getChildren());
+            return userDao.save(newUser);
+        }
     }
 }
