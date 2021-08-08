961f35d1b82b5165ce137dcc3b157a379258adec
Benmessabih Toufik <52804863+toufik7@users.noreply.github.com>
2021-08-08 20:02:34 +0100

    Add files via upload

--- jwtauth/src/main/java/com/esi/jwtauth/controller/UserController.java
+++ jwtauth/src/main/java/com/esi/jwtauth/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.esi.jwtauth.controller;
 
+import com.esi.jwtauth.DTO.DataGlucoseDTO;
+import com.esi.jwtauth.DTO.ResponseTemplate;
+import com.esi.jwtauth.entity.Patient;
 import com.esi.jwtauth.entity.UserDAO;
 import com.esi.jwtauth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -24,12 +28,15 @@ public class UserController {
         public UserDAO getUserById(@PathVariable(name = "id") Long idUsr){
             return userService.getUserById(idUsr);
         }
-        
+
+
+
         @GetMapping("/medecin/{id}/patients")
-        public Collection<UserDAO> getPatientsByMedecin(@PathVariable(name = "id") Long idUsr){
-            UserDAO medecin = userService.getUserById(idUsr);
-            return userService.getUsersByParent(medecin);
-        }
+            public Collection<UserDAO> getPatientsByMedecin(@PathVariable(name = "id") Long idUsr){
+                UserDAO medecin = userService.getUserById(idUsr);
+                return userService.getUsersByParent(medecin);
+            }
+
 
         @PutMapping("/update/{id}")
         public UserDAO updateUser(@RequestBody UserDAO user){
@@ -41,11 +48,28 @@ public class UserController {
         public void deleteUser(@PathVariable(name = "id") Long idUsr){
             userService.deleteUser(idUsr);
         }
-        
+
         // specific to patient -------------------------------------------
         @GetMapping("/patient/{id}")
         public Patient getPatientById(@PathVariable(name = "id") Long idu){
             return userService.getPatientById(idu);
         }
+
+
+        @PutMapping("patient/update/{id}")
+        public Patient updatePatient(@RequestBody Patient patient){
+
+            return userService.updatePatient(patient);
+        }
+        @GetMapping("/patient/{id}/Gly")
+        public List<DataGlucoseDTO> getGlysByPatient(@PathVariable(name = "id") Long idP){
+            return userService.getGlysByPatient(idP);
+        }
+
+        @GetMapping("/get/{username}")
+        public UserDAO getUserByUsername(@PathVariable(name = "username") String username){
+            return userService.getUserByUsername(username);
+        }
+
 
 }
