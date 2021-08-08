56831112d272a37932e4229084a3d3c4b1bbd354
Benmessabih Toufik <52804863+toufik7@users.noreply.github.com>
2021-08-08 20:01:49 +0100

    Add files via upload

--- jwtauth/src/main/java/com/esi/jwtauth/service/UserService.java
+++ jwtauth/src/main/java/com/esi/jwtauth/service/UserService.java
@@ -1,11 +1,19 @@
 package com.esi.jwtauth.service;
 
+import com.esi.jwtauth.DTO.DataGlucoseDTO;
+import com.esi.jwtauth.entity.Patient;
 import com.esi.jwtauth.entity.UserDAO;
+import com.esi.jwtauth.repository.PatientRepository;
 import com.esi.jwtauth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -15,8 +23,13 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
+    private PatientRepository patientRepository;
+
+    @Autowired
     private PasswordEncoder bcryptEncoder;
 
+    @Autowired
+    private RestTemplate restTemplate;
 
 
         //get methods
@@ -27,16 +40,21 @@ public class UserService {
         public UserDAO getUserById(Long idUsr){
             return userRepository.findById(idUsr).orElse(null);
         }
-    
+
+        public UserDAO getUserByUsername(String Usr){
+            return userRepository.findUserDAOByUsername(Usr);
+         }
+
         public Patient getPatientById(Long idu){
-            return patientRepository.findById(idu).orElse(null);
-        }
-    
-        public Collection<UserDAO> getUsersByParent(UserDAO parent){
+        return patientRepository.findById(idu).orElse(null);
+    }
+
+    // get methode Medecin ---------------------------------------------------------
+         public Collection<UserDAO> getUsersByParent(UserDAO parent){
             return userRepository.findUserDAOByParent(parent);
         }
 
-        //delete method
+    //delete method
         public void deleteUser(Long idUsr){
             userRepository.deleteById(idUsr);
             //return "User Removed "+idUsr;
@@ -49,6 +67,36 @@ public class UserService {
             exsistingUser.setPassword(bcryptEncoder.encode(user.getPassword()));
             exsistingUser.setRole(user.getRole());
             exsistingUser.setState(user.getState());
+            exsistingUser.setParent(user.getParent());
+            exsistingUser.setChildren(user.getChildren());
             return userRepository.save(exsistingUser);
         }
+
+
+
+    //update method Patient----not in github yet--------------------------------------------------------
+    public Patient updatePatient(Patient p){
+        Patient exsistingPatient = patientRepository.findById(p.getId()).orElse(null);
+        exsistingPatient.setUsername(p.getUsername());
+        exsistingPatient.setParent(p.getParent());
+        exsistingPatient.setChildren(p.getChildren());
+        exsistingPatient.setNom(p.getNom());
+        exsistingPatient.setPrenom(p.getPrenom());
+        exsistingPatient.setDateNaissance(p.getDateNaissance());
+        exsistingPatient.setGlyId(p.getGlyId());
+        return patientRepository.save(exsistingPatient);
+    }
+
+    public List<DataGlucoseDTO> getGlysByPatient(Long idp){
+        List<DataGlucoseDTO> dataGlucosesList = new ArrayList<>();
+        Patient patient = patientRepository.findById(idp).orElse(null);
+
+        for (Long gly:patient.getGlyId()) {
+            ResponseEntity<DataGlucoseDTO> glyResponse =  restTemplate.exchange(
+                    "http://localhost:9005/iot/"+gly, HttpMethod.GET,null,DataGlucoseDTO.class);
+            DataGlucoseDTO glycemie = glyResponse.getBody();
+            dataGlucosesList.add(glycemie);
+        }
+        return dataGlucosesList;
+    }
 }
